04d845caaa24603cacaf8af87a7af137eecb5789
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2026-01-31 03:40:42 -0300

    feat: Make Main.java accept --file and --output arguments

--- src/main/java/com/gabrielaraujo/iaax/Main.java
+++ src/main/java/com/gabrielaraujo/iaax/Main.java
@@ -1,17 +1,33 @@
-package com.gabrielareaujo.iaax;
+package com.gabrielaraujo.iaax;
+
+import com.gabrielaraujo.iaax.aws.AwsTranspiler;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String[] args) throws IOException {
+        String inputFile = null;
+        String outputFile = null;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < args.length; i++) {
+            if ("--file".equals(args[i]) && i + 1 < args.length) {
+                inputFile = args[++i];
+            } else if ("--output".equals(args[i]) && i + 1 < args.length) {
+                outputFile = args[++i];
+            }
         }
+
+        if (inputFile == null || outputFile == null) {
+            System.err.println("Usage: java com.gabrielaraujo.iaax.Main --file <input-file> --output <output-file>");
+            System.exit(1);
+        }
+
+        var file = AwsTranspiler.transpile(inputFile);
+        Path output = Path.of(outputFile);
+        Files.writeString(output, file);
     }
 }
