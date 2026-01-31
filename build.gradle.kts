06e5387fb4c42074ee15d386b1b277ed68b729f3
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2026-01-31 02:07:39 -0300

    ci: jackson and lombok dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,11 +5,21 @@ plugins {
 group = "com.gabrielaraujo.iaax"
 version = "1.0-SNAPSHOT"
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(11))
+    }
+}
+
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-xml:2.17.2")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.2")
+    compileOnly("org.projectlombok:lombok:1.18.24")
+    annotationProcessor("org.projectlombok:lombok:1.18.24")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
